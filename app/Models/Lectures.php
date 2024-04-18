75a65566522af118506671416e14e8d60296c564
Kima018 <marijan.dev@gmail.com>
2024-04-18 16:24:18 +0200

    * Added Controller and Model for Lectures

---
+++ app/Models/Lectures.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lectures extends Model
+{
+    protected $table = 'lectures';
+    protected $fillable = [
+        'name',
+        'description',
+        'lecture_link',
+        'user_id',
+    ];
+}
