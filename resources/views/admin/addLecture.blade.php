2b7c20d19b9be98b1c708c68423c0b5b8561d071
Kima018 <marijan.dev@gmail.com>
2024-04-18 16:25:02 +0200

    * Making Admin Blades

---
+++ resources/views/admin/addLecture.blade.php
@@ -0,0 +1,9 @@
+@extends('admin.adminLayout')
+
+@section('addLecture')
+    <section class="px-5 sm:ml-64 sm:mt-10">
+        <h1>ADD new lecture</h1>
+
+
+    </section>
+@endsection
---
+++ resources/views/admin/allLectures.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.adminLayout')
+
+@section('allLectures')
+<section class="px-5 sm:ml-64 sm:mt-10">
+    <h1>TEST</h1>
+    <h1>TEST</h1>
+    <h1>TEST</h1>
+    <h1>TEST</h1>
+
+
+</section>
+@endsection
