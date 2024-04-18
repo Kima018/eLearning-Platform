ecb88eaed371bfc1b66330122ec62449d729e663
Kima018 <marijan.dev@gmail.com>
2024-04-18 17:09:38 +0200

    * Added function to save new lecture

--- app/Http/Controllers/LecturesController.php
+++ app/Http/Controllers/LecturesController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Lectures;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class LecturesController extends Controller
@@ -15,6 +18,25 @@ class LecturesController extends Controller
     public function allLectures():View
     {
         return view('admin.allLectures');
+    }
+
+    public function saveNewLecture(Request $request)
+    {
+        $request->validate([
+            'lecture_name'=>'required|string|max:100',
+            'lecture_description'=>'required|string',
+            'lecture_link'=>'required|string'
+        ]);
+
+        Lectures::create([
+            'name'=>$request->get('lecture_name'),
+            'description'=>$request->get('lecture_name'),
+            'lecture_link'=>$request->get('lecture_name'),
+            'user_id'=> Auth::id(),
+        ]);
+
+
+
     }
 
 }
