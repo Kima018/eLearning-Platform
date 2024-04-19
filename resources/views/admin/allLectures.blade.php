fdb0bb578e7a5cb838e5dfe7ac1a45b4178e2675
Kima018 <marijan.dev@gmail.com>
2024-04-19 18:46:01 +0200

    * Added more style

--- resources/views/admin/allLectures.blade.php
+++ resources/views/admin/allLectures.blade.php
@@ -4,7 +4,7 @@
     <section class="px-5 sm:ml-64 sm:mt-10">
         <h1>ALL LECTURES</h1>
 
-        <div class="grid grid-cols-3  gap-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             @if($allLectures->count() > 0 )
                 @foreach($allLectures as $lecture)
                     <div
