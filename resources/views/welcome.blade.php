c42e7f55d49f31a08082e8df8c5f5c4a92cd070b
Kima018 <marijan.dev@gmail.com>
2024-04-19 15:17:30 +0200

    * code eddited

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@if(session()->has('message'))
+    Poruka: {{session('message')}}
+@endif
+
+test
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Http\Controllers\LecturesController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\AdminCheck;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('admin/allLectures');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -17,20 +19,20 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::middleware(['auth', \App\Http\Middleware\AdminCheck::class])->prefix('admin')->group(function () {
-    Route::get('/add-new-lecture', [\App\Http\Controllers\LecturesController::class, 'addNewLecture'])->name('lecture.add');
-    Route::get('/all-lectures', [\App\Http\Controllers\LecturesController::class, 'allLectures'])->name('lecture.all');
+Route::middleware(['auth', AdminCheck::class])->prefix('admin')->group(function () {
+    Route::get('/add-new-lecture', [LecturesController::class, 'addNewLecture'])->name('lecture.add');
+    Route::get('/all-lectures', [LecturesController::class, 'allLectures'])->name('lecture.all');
 
-    Route::post('/save-new-product', [\App\Http\Controllers\LecturesController::class, 'saveNewLecture'])->name('lecture.save');
+    Route::post('/save-new-product', [LecturesController::class, 'saveNewLecture'])->name('lecture.save');
 });
 
 
 require __DIR__ . '/auth.php';
 
 //Napravi ovako:
-//- Authentikacija
-//- ⁠Role admin, moderator i user
-//- ⁠Sistem za dodavanje predavanja
-//- ⁠	- Admin moze da dodaje predavanja, edituje i brise
-//- ⁠- Moderator moze da samo edituje predavanja
-//- ⁠- User moze da samo gleda predavanja
+//- Authentikacija DONE
+//- Role admin, moderator i user
+//- Sistem za dodavanje predavanja (+dodavanje slika)
+//- 	- Admin moze da dodaje predavanja, edituje i brise
+//- - Moderator moze da samo edituje predavanja
+//- - User moze da samo gleda predavanja
